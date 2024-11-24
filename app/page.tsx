f3ae2db47a6dd9eff7cd9181f1d72df1444b507f
Luis <luispaulo.ni@gmail.com>
2024-11-24 11:43:28 -0300

    removed 3 year

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Olá</h1>
       <p className="mb-4">
-        {`Meu nome é Luis, sou desenvolvedor web há 3 anos e gostaria de compartilhar alguns dos meus trabalhos com você.`}
+        {`Meu nome é Luis, sou desenvolvedor web e gostaria de compartilhar alguns dos meus trabalhos com você.`}
       </p>
       <div className="my-8">
         <BlogPosts />
